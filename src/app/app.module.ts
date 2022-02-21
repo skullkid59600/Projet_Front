772b38a5835f30154120ba5f1d4bef5c0f73bbc2
Chiara <brichot.chiara@yahoo.fr>
2022-02-21 12:29:06 +0100

    :sparkles: : index, header, footer, notfound components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule
