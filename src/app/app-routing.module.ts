7aed44cbe1de1aa53c28d61aee91f56ceffe3798
Chiara <brichot.chiara@yahoo.fr>
2022-03-11 02:13:39 +0100

    :sparkles: : routing fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 import {IndexComponent} from "./index/index.component";
-import {FooterComponent} from "./footer/footer.component";
 import {NotfoundComponent} from "./notfound/notfound.component";
+import {GymnastsComponent} from "./gymnasts/gymnasts.component";
+import {TeamsComponent} from "./teams/teams.component";
+import {NewsComponent} from "./news/news.component";
+import {FaqComponent} from "./faq/faq.component";
 
 const routes: Routes = [
   {path: 'index', component: IndexComponent},
-  {path: 'notfound', component: NotfoundComponent}
+  {path: 'notfound', component: NotfoundComponent},
+  {path: 'gymnasts', component: GymnastsComponent},
+  {path: 'teams', component: TeamsComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: '', component: IndexComponent},
+  {path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
